6a558ec3c773646d32c948065dcd6ea347eacf5a
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-04 21:56:02 +0400

    создали программу которая на вход принимает два числа и проверяет является ли первое число квадратом второго

---
+++ ex02/Program.cs
@@ -0,0 +1,21 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение 
+    string readValue = Console.ReadLine();// считывает с консоли строку
+    int resault = int.Parse(readValue);// преобразует строку в целое число
+    return resault; // возвращает результат
+}
+
+int numberOne = Prompt("Введите первое число:");
+int numberTwo = Prompt ("Введите второе число:");
+
+int resault = numberOne * numberOne;
+if (resault == numberTwo)
+{
+    System.Console.WriteLine($"Число {numberOne} является квадратом числа {numberTwo}");
+}
+
+else 
+{
+    System.Console.WriteLine($"Число {numberOne} не является квадратом числа {numberTwo}");
+}
