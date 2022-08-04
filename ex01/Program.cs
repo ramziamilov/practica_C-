4ec827b883c5a90535b64c0cb01ac20e8f16ae53
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-04 21:35:02 +0400

    создали программу для возведения в квадрат

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//программа которая на входе принимает число и выводит его квадрат
+
+// Функция, на вход получает параметр, на выходе введенное с консоли значение
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение 
+    string readValue = Console.ReadLine();// считывает с консоли строку
+    int resault = int.Parse(readValue);// преобразует строку в целое число
+    return resault; // возвращает результат
+}
+
+int value = Prompt("Введите число:"); // вводим значение
+int resault = value * value; // вычисляем квадрат
+System.Console.WriteLine($"Квадрат числа {value} равен {resault}"); // вывод результата
