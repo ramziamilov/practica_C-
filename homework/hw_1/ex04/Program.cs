254ec96cd410532add2638469ca425d3cdd25f5b
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-07 22:43:01 +0400

    make ex04 hw

---
+++ homework/hw_1/ex04/Program.cs
@@ -0,0 +1,17 @@
+﻿//принмаем число Н и выводим интрвал четных чисел от 1  до Н
+
+
+System.Console.Write("Введите любое число: ");
+string value = Console.ReadLine();
+int number = int.Parse(value);
+
+int count = 1;
+
+while (count < number)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.Write($" {count} ");
+    }
+    count++;
+}
