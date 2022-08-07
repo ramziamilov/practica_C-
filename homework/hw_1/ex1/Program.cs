99deb015ae8eff6afddc669daac4a0eac5722ab7
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-07 21:33:48 +0400

    make ex01 hw

---
+++ homework/hw_1/ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа: принимем на вход два числа и выдаем какео больше, а какое меньше
+
+
+System.Console.Write("Введите первое число: ");
+string valueOne = Console.ReadLine();
+System.Console.Write("Введите второе число: ");
+string valueTwo = Console.ReadLine();
+int numberOne = int.Parse(valueOne); // преобразовали строки в числа
+int numberTwo = int.Parse(valueTwo);
+
+if (numberOne > numberTwo)
+{
+    System.Console.WriteLine($"max = {numberOne}, min = {numberTwo}");
+}
+else
+{
+  System.Console.WriteLine($"max = {numberTwo}, min = {numberOne}");
+}
