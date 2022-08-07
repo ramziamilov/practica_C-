454dca4da74f1de15b4eb408d3a4ee26563f1645
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-07 22:04:30 +0400

    make ex02 hw

---
+++ homework/hw_1/ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿System.Console.Write("Введите первое число: ");
+string valueOne = Console.ReadLine();
+System.Console.Write("Введите второе число: ");
+string valueTwo = Console.ReadLine();
+System.Console.Write("Введите третье число: ");
+string valueThree = Console.ReadLine();
+
+int numberOne = int.Parse(valueOne);
+int numberTwo = int.Parse(valueTwo);
+int numberThree = int.Parse(valueThree);
+
+if (numberOne > numberTwo)
+{
+    if (numberTwo > numberThree)
+    {
+        System.Console.WriteLine($"Максимальное число = {numberOne}");
+    }
+}
+else
+{
+    if (numberTwo > numberThree)
+    {
+        System.Console.WriteLine($"Максимальное число = {numberTwo}");
+    }
+
+    else
+    {
+        System.Console.WriteLine($"Максимальное число = {numberThree}");
+    }
+}
