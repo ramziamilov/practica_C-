febdb176392a643f9752b712177af2436fd71c2b
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 22:21:21 +0400

    прога выводит случайное число и макс его цифру

---
+++ lesson2/ex05/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 9999] и показывает наибольшую цифру числа
+
+
+int number = new Random().Next(10, 10000);
+System.Console.WriteLine(number);
+
+int max = 0;
+
+while (number > 0)
+{
+    int a = number % 10;
+    if (max < a)
+    {
+        max = a;
+    }
+    number=number/10;
+    
+}
+System.Console.WriteLine(max);
