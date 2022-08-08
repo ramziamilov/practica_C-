70e8bc4a865730c437d95ede33340ba243573dda
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 21:53:31 +0400

    программа показывавет случайное число от 10 до 99

---
+++ lesson2/ex02/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 99);
+System.Console.WriteLine(number);
+
+int numberOne = number % 10;
+int numbertwo = number / 10;
+
+if (numberOne> numbertwo)
+{
+    System.Console.WriteLine(numberOne);
+}
+else 
+{
+    System.Console.WriteLine(numbertwo);
+}
