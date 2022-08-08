ef5b049ee70eb80cdf9b72a02252ee1c1725872b
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 22:50:07 +0400

    программа которая принимает числа и выводит сумму через массивы

---
+++ lesson2/ex06/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет принимать на вход пять чисел 
+// и выводить сумму и среднее арифметическое этих чисел.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);           // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+
+int[] inputarray(int Length)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i} элемент");
+    }
+    return array;
+}
+int Summ(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        summ = summ + array[i];
+    }
+    return summ;
+}
+int[] array = inputarray(3);
+int summ = Summ(array);
+double average = summ / ((double)array.Length);
+System.Console.WriteLine($"Сумма равна {summ}, среднее равно {average}");
