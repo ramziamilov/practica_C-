da375dc342b3d8abb8ec686d414e985f9485fc08
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 22:05:43 +0400

    прога принимает два числа и и проверяет кратность

---
+++ lesson2/ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение 
+    string readValue = Console.ReadLine();// считывает с консоли строку
+    int resault = int.Parse(readValue);// преобразует строку в целое число
+    return resault; // возвращает результат
+}
+
+
+int num1 = Prompt("Введите первое число:"); // вводим значение
+int num2 = Prompt ("Введите второе число:");
+int ost;
+ost = num1%num2;
+if (ost == 0)
+{
+System.Console.WriteLine("кратно");
+}
+else 
+{
+    System.Console.WriteLine($"не кратно, остаток {ost}");
+}
