c6da17d20fa00ff44c5abae5fd26b13afdb285c8
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 22:11:24 +0400

    число кротано 7 и 23 сразу

---
+++ lesson2/ex04/Program.cs
@@ -0,0 +1,18 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение 
+    string readValue = Console.ReadLine();// считывает с консоли строку
+    int resault = int.Parse(readValue);// преобразует строку в целое число
+    return resault; // возвращает результат
+}
+
+
+int num1 = Prompt("Введите первое число:"); // вводим значение
+if (num1 % 7 == 0 && num1 % 23 == 0)
+{
+    System.Console.WriteLine("da");
+}
+else
+{
+    System.Console.WriteLine("HeT");
+}
