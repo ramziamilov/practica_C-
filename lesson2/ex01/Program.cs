ed76c0b2d54960d82d5d5b1cb462dfd6fba8ff25
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 21:46:12 +0400

    программа выводит последнюю цифру числа

---
+++ lesson2/ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение 
+    string readValue = Console.ReadLine();// считывает с консоли строку
+    int resault = int.Parse(readValue);// преобразует строку в целое число
+    return resault; // возвращает результат
+}
+int value = Prompt("Введите трехзначное число:"); // вводим значение
+int result = value%10;
+System.Console.WriteLine(result);
+
+
