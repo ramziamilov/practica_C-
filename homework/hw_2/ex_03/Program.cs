6423c992ea237ed8338d784bd9bae4cf7d90ae63
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-11 17:58:31 +0400

    вывщдим третье число из заданного или говорим нет

---
+++ homework/hw_2/ex_03/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);           // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+
+int value = Prompt("ВВедите число: ");
+if (value > 99)
+{
+
+    while (value > 1000)
+    {
+        value = value / 10;
+    }
+    int result = value % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
