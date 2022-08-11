fc5c31020514785fc5899517ac2f7a22bde9ac6e
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-11 16:15:57 +0400

    программа удаляет второе число из трехначного

---
+++ homework/hw_2/ex_02/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int value = new Random().Next(100, 1000);
+System.Console.WriteLine(value);
+
+int three = value % 10;
+int two = value / 10 % 10;
+int one = value / 100 % 10;
+System.Console.WriteLine($"{one}{three}");
