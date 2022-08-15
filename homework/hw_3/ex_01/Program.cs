19a0d20c1efa42de6be3cd4d4d59a667c62c40c1
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-15 19:01:56 +0400

    проверка полиндрома 5 числа

---
+++ homework/hw_3/ex_01/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+
+int[] arrayNew(string massage) // функция перевода числа в массив из цифр
+{
+    System.Console.Write(massage);
+    string input = System.Console.ReadLine().ToString();
+    int[] array = new int[input.Length];
+    for (int i = 0; i < input.Length; i++)
+    {
+        array[i] = int.Parse(input[i].ToString());
+
+    }
+    return array;
+}
+
+int [] chekPolindrom (int [] array)
+{
+if (array[0] > 0 && array.Length == 5)
+{
+    if (array[0] == array[4] && array[1] == array[3])
+    {
+        System.Console.WriteLine( "Палиндром");
+    }
+    else
+    {
+        System.Console.WriteLine("Не палиндром");
+    } 
+}
+else {
+    System.Console.WriteLine("некоректные данные");
+}
+return array;
+}
+
+int[] number = arrayNew("Введите пятизначное число: ");
+chekPolindrom (number);
+
+
+
+
+
