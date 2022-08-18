49932a44a97a754068f478df99d9153581c89767
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-18 19:08:43 +0400

    вводим два числа и возводим в натуральную степень

---
+++ homework/hw_4/ex_01/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B. 3, 5 -> 243 (3⁵);  2, 4 -> 16
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = System.Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int DeegreOfNumber(int numOne, int numTwo)
+{
+    if (numTwo > 0)
+    {
+        int value= 1;
+        for (int i = 0; i < numTwo; i++)
+        {
+           value = numOne*value;
+        }
+         return value;
+    }
+    else
+    {
+        Console.WriteLine("некорректный ввод");
+    return 0;
+    }
+
+
+   
+}
+
+int numOne = Prompt("Введите число: ");
+int numTwo = Prompt("Введите второе число: ");
+int result = DeegreOfNumber(numOne, numTwo);
+Console.WriteLine(result);
