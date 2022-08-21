5824fb8cb01712e9bf66c1628fc7c37b41a4f1b8
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-21 09:13:29 +0400

    Принимаем число и выдаем сумму цифр

--- homework/hw_4/ex_02/Program.cs
+++ homework/hw_4/ex_02/Program.cs
@@ -1,2 +1,30 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 452 -> 11; 82 -> 10; 9012 -> 12
 
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = System.Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int Sum(int number)
+{
+    int result = 0;
+    
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число: ");
+if (number>=0)
+{
+System.Console.WriteLine(Sum(number));
+}
+else {
+    System.Console.WriteLine("некорректный ввод");
+}
