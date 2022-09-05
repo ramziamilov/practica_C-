0f465e4b80482a13df0c105a9542f4b95396267e
Ramazan_Zyamilov <nash96@bk.ru>
2022-09-05 20:13:58 +0400

    среднее арифмитическое столбцов матрицы

---
+++ homework/hw_7/ex_03/Program.cs
@@ -0,0 +1,60 @@
+﻿//Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void FindAvg(int[,] matr)
+{
+    double sum = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i, j];
+        }
+        double avg = sum / matr.GetLength(0);
+        Console.Write($"{avg:f2} ");
+    }
+}
+
+
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int[,] matrix = new int[rows, columns];
+FillArray(matrix);
+PrintArray(matrix);
+System.Console.WriteLine("Среднее арифмитическое столбцов: ");
+FindAvg(matrix);
