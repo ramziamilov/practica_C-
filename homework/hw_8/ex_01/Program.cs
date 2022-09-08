444a472a2c224d60251a74d12956720e3d72ca25
Ramazan_Zyamilov <nash96@bk.ru>
2022-09-08 09:45:36 +0400

    упорядочивание строк массив

---
+++ homework/hw_8/ex_01/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] OrderingArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1 - k; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int tmp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = tmp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int[,] matrix = new int[rows, columns];
+FillArray(matrix);
+PrintArray(matrix);
+int[,] matr = OrderingArray(matrix);
+System.Console.WriteLine();
+PrintArray(matr);
