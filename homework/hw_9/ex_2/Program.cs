51fbfe6f06bb1829493408188640a9146551eded
Ramazan_Zyamilov <nash96@bk.ru>
2022-09-14 22:50:23 +0400

    сумма натруальных элементов в промежутке рекурсией

---
+++ homework/hw_9/ex_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int SumNaturalNumbers(int min, int max)
+{
+
+    if (max < min || max < 0)
+    {
+        return 0;
+    }
+    return SumNaturalNumbers(min, max - 1) + max;
+
+}
+
+int min = Prompt("Введите левую границу ряда чисел: ");
+int max = Prompt("Введите правую границу ряда чисел: ");
+int sum = SumNaturalNumbers(min, max);
+Console.WriteLine(sum);
